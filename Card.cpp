ee83829dbaca70fbce731f9b67ddeb6901d0d0f6
Bovsunovskydima <bovsdmitrij1515@gmail.com>
2025-09-07 13:42:16 +0300

    add validation for Card rank and suit

--- Card.cpp
+++ Card.cpp
@@ -1,4 +1,14 @@
 #include "Card.h"
 #include <string>
 
-Card::Card(int suit, int rank) : suit(suit), rank(rank) {}
+Card::Card(int suit, int rank) : suit(suit), rank(rank)
+{
+    if (rank < 1)
+    {
+        throw std::invalid_argument("Card rank must be at least 1.");
+    }
+    if (suit < 0)
+    {
+        throw std::invalid_argument("Card suit cannot be negative.");
+    }
+}
