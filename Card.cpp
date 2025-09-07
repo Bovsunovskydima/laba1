fab7eb83f5169bfd723bc8841af4c50a905ca0bf
Bovsunovskydima <bovsdmitrij1515@gmail.com>
2025-09-07 13:55:39 +0300

    implement operators

--- Card.cpp
+++ Card.cpp
@@ -12,3 +12,37 @@ Card::Card(int suit, int rank) : suit(suit), rank(rank)
         throw std::invalid_argument("Card suit cannot be negative.");
     }
 }
+
+std::strong_ordering Card::operator<=>(const Card &other) const
+{
+    return rank <=> other.rank;
+}
+
+std::ostream &operator<<(std::ostream &os, const Card &card)
+{
+    std::string rankStr;
+    if (card.rank == 1)
+    {
+        rankStr = "A";
+    }
+    else if (card.rank == 11)
+    {
+        rankStr = "J";
+    }
+    else if (card.rank == 12)
+    {
+        rankStr = "Q";
+    }
+    else if (card.rank == 13)
+    {
+        rankStr = "K";
+    }
+    else
+    {
+        rankStr = std::to_string(card.rank);
+    }
+
+    const char *suits[] = {"S", "H", "D", "C"}; // Spades, Hearts, Diamonds, Clubs
+    os << rankStr << suits[card.suit % 4];
+    return os;
+}
